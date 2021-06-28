208f25cd3473b7dbdcbf40609aea91ce93be9851
kbh84 <kais.bouhamed@gmail.com>
2021-06-28 16:01:45 +0100

    1st commit

---
+++ app.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+int main() {
+        int i;
+        for(i=0;i<10;i++) {
+                printf("Hello DevOps \n ");
+                sleep(3);
+        }
+        return 0;
+}
